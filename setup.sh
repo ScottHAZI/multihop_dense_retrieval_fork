5337fab5cc8dc0a5fd533712f4547997f178e8a3
ScottHAZI <250400111@qq.com>
2023-08-09 00:07:12 +0800

    update setup.sh and eval_mhop_retrieval

--- setup.sh
+++ setup.sh
@@ -5,15 +5,12 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
-set -e
-
 pip install -r requirements.txt
-#conda install faiss-cpu -c pytorch
-#conda install faiss-gpu cudatoolkit=10.2 -c pytorch
-#conda install pytorch cudatoolkit=10.2 -c pytorch
+# conda install -c pytorch faiss-cpu=1.7.4 mkl=2021 blas=1.0=mkl
+conda install faiss-gpu=1.7.4 mkl=2021 pytorch=2.0.1 pytorch-cuda=11.8 numpy -c pytorch -c nvidia
 
-#git clone https://github.com/NVIDIA/apex --branch 23.05 --single-branch
-#cd apex
-#pip install -v --disable-pip-version-check --no-cache-dir ./
+git clone git@github.com:NVIDIA/apex.git --branch 23.05 --single-branch
+cd apex
+pip install -v --disable-pip-version-check --no-cache-dir ./
 
 python setup.py develop
